35d87e1b91f0d3aba9c056eec568f7a8ab9a0eb9
Lucas Amaral <analista.sistemas.lucas@gmail.com>
2023-12-20 01:31:29 +0100

    feat: useFocusEffect async Storage

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,13 @@
 import { FlatList } from 'react-native'
-import { useState } from 'react'
-import { useNavigation } from '@react-navigation/native'
+import { useState, useCallback } from 'react'
+import { useNavigation, useFocusEffect } from '@react-navigation/native'
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight'
 import { Container } from './styles'
 import { GroupCard } from '@components/GroupCard';
 import { ListEmpty } from '@components/ListEmpty'
 import { Button } from '@components/Button'
+import { groupsGetAll } from '@storage/group/groupsGetAll';
 
 export function Groups() {
     const [groups, setGroups] = useState<string[]>([])
@@ -16,6 +17,21 @@ export function Groups() {
     function handleNewGroup() {
         navigation.navigate('new')
     }
+
+    async function fetchGroups() {
+        try {
+            const data = await groupsGetAll()
+
+            setGroups(data)
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useFocusEffect(useCallback(() => {
+        fetchGroups()
+    }, []))
 
     return (
         <Container>
