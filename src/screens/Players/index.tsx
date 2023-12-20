06c8b1db3b4f94b5cdfdf54e104e44b2fd3f1431
Lucas Amaral <analista.sistemas.lucas@gmail.com>
2023-12-20 20:30:42 +0100

    feat: crud com Async Storage

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,26 +1,111 @@
-import { FlatList } from 'react-native';
+import { useState, useEffect, useRef } from 'react';
+import { Alert, FlatList, TextInput, Keyboard } from 'react-native';
 import { Header } from '@components/Header'
 import { Highlight } from "@components/Highlight";
-import { useRoute } from '@react-navigation/native'
+import { useRoute, useNavigation } from '@react-navigation/native'
 import { Container, Form, HeaderList, NumbersOfPlayers } from './styles'
 import { ButtonIcon } from '@components/ ButtonIcon';
 import { Input } from '@components/Input';
 import { Filter } from '@components/Filter';
-import { useState } from 'react';
 import { PlayerCard } from '@components/PlayerCard';
 import { ListEmpty } from '@components/ListEmpty';
 import { Button } from '@components/Button';
+import { AppError } from '@utils/AppError';
+import { playerAddByGroup } from '@storage/player/playerAddByGroup';
+import { playersGetByGroup } from '@storage/player/playersGetByGroup';
+import { playersGetByGroupAndTeam } from '@storage/player/playersGetByGroupAndTeam';
+import { PlayerStorageDTO } from '@storage/player/PlayerStorageDTO';
+import { playerRemoveByGroup } from '@storage/player/ playerRemoveByGroup';
+import { groupRemoveByName } from '@storage/group/groupRemoveByName';
 
 type RouteParams = {
     group: string;
 }
 
 export function Players() {
+    const [newPlayerName, setNewPlayerName] = useState('')
     const [team, setTeam] = useState('Time A');
-    const [players, setPlayers] = useState([]);
+    const [players, setPlayers] = useState<PlayerStorageDTO[]>([]);
 
     const route = useRoute()
+    const navigation = useNavigation()
+
     const { group } = route.params as RouteParams
+
+    const newPlayerNameInputRef = useRef<TextInput>(null)
+
+    async function handleAddPlayer() {
+        if (newPlayerName.trim().length === 0) {
+            return Alert.alert('Nova pessoa', 'Informe um nome.')
+        }
+
+        const newPlayer = {
+            name: newPlayerName,
+            team
+        }
+
+        try {
+            await playerAddByGroup(newPlayer, group)
+
+            newPlayerNameInputRef.current?.blur()
+            Keyboard.dismiss()
+
+            setNewPlayerName('')
+            fetchPlayersByTeam()
+
+        } catch (error) {
+            if (error instanceof AppError) {
+                Alert.alert('Nova pessoa', error.message)
+            } else {
+                Alert.alert('Nova pessoa', 'Não foi possível adicionar.')
+            }
+        }
+    }
+
+    async function fetchPlayersByTeam() {
+        try {
+            const playersByTeam = await playersGetByGroupAndTeam(group, team)
+            setPlayers(playersByTeam)
+
+        } catch (error) {
+            console.log(error)
+            Alert.alert('Pessoas', 'Não foi possível carregar as pessoas do time selecionado.')
+        }
+    }
+
+    async function handlePlayerRemove(playerName: string) {
+        try {
+            await playerRemoveByGroup(playerName, group)
+            fetchPlayersByTeam()
+
+        } catch (error) {
+            Alert.alert('Remover pessoa', 'Não foi possível remover essa pessoa.')
+        }
+    }
+
+    async function groupRemove() {
+        try {
+            await groupRemoveByName(group)
+            navigation.navigate('groups')
+
+        } catch (error) {
+            console.log(error)
+            Alert.alert('Remover grupo', 'Nao foi possivel remover o grupo.')
+        }
+    }
+
+    async function handleGroupRemove() {
+        Alert.alert("Remover", "Deseja remover o grupo ?",
+            [
+                { text: 'Não', style: 'cancel' },
+                { text: 'Sim', onPress: () => groupRemove() }
+            ]
+        )
+    }
+
+    useEffect(() => {
+        fetchPlayersByTeam()
+    }, [team])
 
     return (
         <Container>
@@ -32,11 +117,17 @@ export function Players() {
             />
             <Form>
                 <Input
+                    inputRef={newPlayerNameInputRef}
+                    onChangeText={setNewPlayerName}
+                    value={newPlayerName}
                     placeholder='Nome da pessoa'
                     autoCorrect={false}
+                    onSubmitEditing={handleAddPlayer}
+                    returnKeyType='done'
                 />
                 <ButtonIcon
                     icon="add"
+                    onPress={handleAddPlayer}
                 />
             </Form>
             <HeaderList>
@@ -58,11 +149,11 @@ export function Players() {
             </HeaderList>
             <FlatList
                 data={players}
-                keyExtractor={item => item}
+                keyExtractor={item => item.name}
                 renderItem={({ item }) => (
                     <PlayerCard
-                        name={item}
-                        onRemove={() => { }}
+                        name={item.name}
+                        onRemove={() => handlePlayerRemove(item.name)}
                     />
                 )}
                 ListEmptyComponent={() => (
@@ -79,6 +170,7 @@ export function Players() {
             <Button
                 title="Remover Turma"
                 type="SECONDARY"
+                onPress={handleGroupRemove}
             />
         </Container>
     );
